45838261f5cdcd002e2a5a72b76430dec6f02217
Joshua Gould <joshgould24@gmail.com>
2022-01-23 14:14:34 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'another_test_app_angular',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
